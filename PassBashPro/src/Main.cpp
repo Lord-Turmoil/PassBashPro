3bd788db8fa9d0c95b48d9637312ad284ad47e00
Turmoil <tony-turmoil@qq.com>
2023-04-09 23:42:00 +0800

    feat: add Main.cpp

---
+++ PassBashPro/src/Main.cpp
@@ -0,0 +1,41 @@
+/******************************************************************************
+ ***                        T O N Y ' S  S T U D I O                        ***
+ ******************************************************************************
+ *                   Project Name : PassBashPro                               *
+ *                                                                            *
+ *                      File Name : Main.cpp                                  *
+ *                                                                            *
+ *                     Programmer : Tony Skywalker                            *
+ *                                                                            *
+ *                     Start Date : April 9, 2023                             *
+ *                                                                            *
+ *                    Last Update :                                           *
+ *                                                                            *
+ * -------------------------------------------------------------------------- *
+ * Over View:                                                                 *
+ *   Main entrance of PassBash.                                               *
+ * -------------------------------------------------------------------------- *
+ * Build Environment:                                                         *
+ *   Windows 11 Pro                                                           *
+ *   Visual Studio 2022 Community Preview                                     *
+ ******************************************************************************/
+
+#include "../inc/common/Macros.h"
+#include "../inc/common/Error.h"
+#include "../inc/common/Logger.h"
+#include "../inc/exec/ExecHost.h"
+#include "../inc/exec/Init.h"
+
+int main(int argc, char* argv[])
+{
+	InitExecHost();
+
+	PASH_PANIC_ON(ExecHost::GetInstance()->execl(EXEC_HIDDEN, "start", nullptr));
+
+#ifdef PASH_DEBUG
+	LOG_PRINT_ERROR();
+	LOG_PRINT_MESSAGE();
+#endif
+
+	return 0;
+}
