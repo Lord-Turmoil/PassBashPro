ffcb95ae12c31d413f04ff72cfa0bab73fc4c74a
Turmoil <tony-turmoil@qq.com>
2023-04-11 20:36:31 +0800

    feat: complete editor aux

--- PassBashPro/inc/exec/editor/Editor.h
+++ PassBashPro/inc/exec/editor/Editor.h
@@ -29,14 +29,7 @@
 #include <string>
 #include <cnsl.h>
 
-// static void _edit_usage()
-// static int _edit_parse_args(int argc, char* argv[], std::string& path)
-// static void _edit_parse_cmd(char* cmd, char*& type, char*& arg);
-// static void _edit_print_prompt();
-
 int exec_edit_host(int argc, char* argv[]);
-
-int exec_edit_edit(int argc, char* argv[]);
 // int exec_edit_help(int argc, char* argv[]);
 int exec_edit_clear(int argc, char* argv[]);
 int exec_edit_see(int argc, char* argv[]);
@@ -47,16 +40,5 @@ int exec_edit_setw(int argc, char* argv[]);
 int exec_edit_unset(int argc, char* argv[]);
 int exec_edit_unknown(int argc, char* argv[]);
 int exec_edit_exit(int argc, char* argv[]);
-
-// static int _set_key(const char* idStr, const char* newKey);
-// static int _set_value(const char* idStr, const char* value);
-// static int _set_weight(const char* idStr, const char* weightStr);
-// static int _set_entry(const char* key, const char* value, const char* weightStr);
-// static int _edit_parse_params(char* arg, int argc, const char* params[]);
-// static void _set_usage();
-// static void _setk_usage();
-// static void _setv_usage();
-// static void _setw_usage();
-// static void _unset_usage();
 
 #endif
